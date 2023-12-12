b1c7fb9a673b4718385d78888e9429b1e6384856
ssrinke <ssrinke@gmail.com>
2023-12-12 18:11:42 +0800

    update navbar

--- src/ui/navbar.tsx
+++ src/ui/navbar.tsx
@@ -10,23 +10,27 @@ export default async function Nav() {
           akari
         </Link>
       </div>
-      <div className='flex-none dropdown dropdown-end'>
-        <button tabIndex={0} role='button' className='btn btn-square btn-ghost'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            className='inline-block w-5 h-5 stroke-current'
+      {!!authData?.user ? (
+        <div className='flex-none dropdown dropdown-end'>
+          <button
+            tabIndex={0}
+            role='button'
+            className='btn btn-square btn-ghost'
           >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              d='M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z'
-            ></path>
-          </svg>
-        </button>
-        {!!authData?.user ? (
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              className='inline-block w-5 h-5 stroke-current'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z'
+              ></path>
+            </svg>
+          </button>
           <ul
             tabIndex={0}
             className='mt-3 z-[100] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52'
@@ -42,17 +46,17 @@ export default async function Nav() {
               </li>
             </form>
           </ul>
-        ) : (
-          <ul
-            tabIndex={0}
-            className='mt-3 z-[100] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52'
-          >
-            <li>
-              <Link href={`/cas`}>中央认证登陆(仅限校园网)</Link>
-            </li>
-          </ul>
-        )}
-      </div>
+        </div>
+      ) : (
+        <ul
+          tabIndex={0}
+          className='mt-3 z-[100] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52'
+        >
+          <li>
+            <Link href={`/cas`}>中央认证登陆(仅限校园网)</Link>
+          </li>
+        </ul>
+      )}
     </div>
   )
 }
